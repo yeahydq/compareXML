1191d4ac227683d203fec3e295e09c1a35be08e5
dick <yeahydq@163.com>
2017-02-14 00:23:35 +0800

    add the pdf

--- parseXML.py
+++ parseXML.py
@@ -12,6 +12,7 @@ def iterate_children(parent):
             yield child
         child = child.nextSibling
 
+
 def parseXML(xmlName='/dev/null'):
     xmlFile=minidom.parse(xmlName)
     xmlTree=xmlFile.documentElement
@@ -31,36 +32,28 @@ def parseXML(xmlName='/dev/null'):
         jobList[jobName].update(dict(childDict))
     return jobList
 
-def replaceVer(jobList={},mapps={}):
-    newList=copy.deepcopy(jobList)
-    for job in newList:
-        for key in mapps:
-            if newList[job][key]==mapps[key][1]:
-                newList[job][key]=mapps[key][0]
-    return newList
 
-def replaceDict(newList={},mapps={}):
-    # newList=copy.deepcopy(jobList)
-    for job in newList:
-        for key in mapps:
-            if newList[job][key]==mapps[key][1]:
-                newList[job][key]=mapps[key][0]
-    return newList
+def replaceList(jobList=[], mapps={}):
+    for i in range(len(jobList)):
+        if type(jobList[i]) == dict:
+            replaceDict(jobList[i],mapps)
+        elif type(jobList[i]) == list:
+            replaceList(jobList[i], mapps)
+            pass
 
-def replaceVerAll(jobList=[],mapps={}):
-    newList=copy.deepcopy(jobList)
-    for i in range(len(newList)):
-        if isinstance(newList[i],list):
-            newList=replaceVer(newList,mapps)
-        elif isinstance(newList[i])==dict:
-            for job in newList:
-                for key in mapps:
-                    if isinstance(newList[job][key][1])==dict:
-                        replaceVer(newList[job][key][1],mapps)
-                        pass
-                    if newList[job][key]==mapps[key][1]:
-                        newList[job][key]=mapps[key][0]
-    return newList
+
+def replaceDict(jobList={}, mapps={}):
+    for key in jobList:
+        if type(jobList[key]) == dict:
+            replaceDict(jobList[key],mapps)
+        elif type(jobList[key]) == list:
+            replaceList(jobList[key], mapps)
+
+    for key in mapps:
+        if jobList.get(key) == mapps[key][1]:
+            jobList[key] = mapps[key][0]
+    pass
+
 
 def cmpdicts(dct0, dct1):
     diffs = {}
@@ -68,60 +61,6 @@ def cmpdicts(dct0, dct1):
     for k in keys:
             if cmp(dct0.get(k), dct1.get(k)):
                 diffs[k]=(dct0.get(k),dct1.get(k))
-    return diffs
-
-def cmpDeepDict(dct0, dct1):
-    diffs = {}
-    keys = set(dct0.keys() + dct1.keys())
-    for k in keys:
-            if dct0.get(k)!=dct1.get(k):
-                if dct1.get(k) == None:
-                    diffs[k] = ("DICT Left Insert:",dct0.get('JOBNAME'),k, dct0.get(k))
-                elif dct0.get(k) == None:
-                    diffs[k] = ("DICT Right Insert:",dct1.get('JOBNAME'),k, dct1.get(k))
-                elif isinstance(dct0.get(k),list):
-                    diffs[k] = cmpDeepLists(dct0.get(k),dct1.get(k))
-                elif type(dct0.get(k)) == dict and type(dct1.get(k)) == dict:
-                    diffs[k] = cmpDeepDict(dct0.get(k), dct1.get(k))
-                else:
-                    diffs[k]=("DICT Update:",dct1.get('JOBNAME'),k,dct0.get(k),dct1.get(k))
-
-            # if cmp(dct0.get(k), dct1.get(k)):
-            #     diffs[k]=(dct0.get(k),dct1.get(k))
-    return diffs
-
-def getFrmList(l=[],i=0):
-    if i >= len(l):
-        return None
-    else:
-        return l[min(i,len(l)-1)]
-
-def cmpLists(listA, listB):
-    diffs = []
-    #
-    sortA=sorted(listA)
-    sortB=sorted(listB)
-    print sortA,sortB
-    iA=0
-    iB=0
-    for i in range(max(len(sortA),len(sortB)))*2:
-        if getFrmList(sortA,iA) == getFrmList(sortB,iB):
-            iA+=1
-            iB+=1
-        elif getFrmList(sortA,iA) != getFrmList(sortB,iB):
-            if getFrmList(sortA,iA + 1) == getFrmList(sortB,iB):
-                diffs.append(("Left Insert", sortA[iA]))
-                iA += 1
-            elif getFrmList(sortA,iA) == getFrmList(sortB,iB+1):
-                diffs.append(("Right Insert", getFrmList(sortB,iB)))
-                iB += 1
-            else :
-                # diffs.append(("Update", (getFrmList(sortA, iA),getFrmList(sortB, iB))))
-                diffs.append(("Left Insert", sortA[iA]))
-                diffs.append(("Right Insert", sortB[iB]))
-                iA += 1
-                iB += 1
-    # print list(set(listA).difference(set(listB)))
     return diffs
 
 
@@ -152,10 +91,10 @@ def cmpDeepLists(listA, listB):
             iA+=1
             iB+=1
         elif nxtA == curB:
-                diffs.append(("Left Insert", curA))
+                diffs.append({"Left Insert": curA})
                 iA += 1
         elif curA == nxtB:
-            diffs.append(("Right Insert", curB))
+            diffs.append({"Right Insert": curB})
             iB += 1
         elif curTypA==dict and curTypB==dict and curA.get('NAME') == curB.get('NAME'):
             diffDict=cmpDeepDict(curA,curB)
@@ -169,26 +108,49 @@ def cmpDeepLists(listA, listB):
             iB+=1
         else:
             if curTypA != dict and nxtTypA==dict:
-                diffs.append(("Left Insert", curA))
+                diffs.append({"Left Insert": curA})
                 iA += 1
             if curTypB != dict and nxtTypB==dict:
-                diffs.append(("Right Insert", curB))
+                diffs.append({"Right Insert", curB})
                 iB += 1
         # if nxtA==None and nxtB==None:
         #     break
     # print list(set(listA).difference(set(listB)))
     return diffs
 
+def cmpDeepDict(dct0, dct1,keyFld="JOBNAME"):
+    diffs = {}
+    keys = set(dct0.keys() + dct1.keys())
+    for k in keys:
+        if dct0.get(k)!=dct1.get(k):
+            if dct1.get(k) == None:
+                # diffs[k] = ({"Left Insert": (dct0.get(keyFld),k, dct0.get(k))})
+                # JOBNAME=JOB001 Field="123"
+                diffs[k] = {"Left Insert": 'where %s="%s", string %s="%s" is added' % (keyFld,dct0.get(keyFld),k, dct0.get(k))}
+            elif dct0.get(k) == None:
+                diffs[k] = {"Right Insert":'where %s="%s", string %s="%s" is added' % (keyFld,dct1.get(keyFld),k, dct1.get(k))}
+            elif k==keyFld:
+                diffs[k] = [
+                            {"Left Insert": 'where %s="%s", string %s="%s" is added' % (keyFld,dct0.get(keyFld),k, dct0.get(k))},
+                            {"Right Insert": 'where %s="%s", string %s="%s" is added' % (keyFld,dct1.get(keyFld),k, dct1.get(k))}
+                            ]
+                # diffs[k] = ("DICT RIGHT Insert:", dct1.get(keyFld), k, dct1.get(k))
+            elif isinstance(dct0.get(k),list):
+                diffs[k] = cmpDeepLists(dct0.get(k),dct1.get(k))
+            elif type(dct0.get(k)) == dict and type(dct1.get(k)) == dict:
+                diffs[k] = cmpDeepDict(dct0.get(k), dct1.get(k),keyFld)
+            else:
+                diffs[k]={"Change": 'where %s="%s", variable:%s changes from left="%s" to right="%s"' % (keyFld,dct1.get(keyFld),k,dct0.get(k),dct1.get(k))}
+    return diffs
 
-a=[0,1,{"NAME":4,'VAR1':1},{"NAME":4,'VAR2':1},4]
-b=[3,{"NAME":4,'VAR1':2},4,1,5]
-# print cmpDeepLists(a,b)
-# sys.exit(0)
 
-mapping={
-    "NODE_ID":('PRD.SERVER','UAT.SERVER'),
-    "USER_ID": ('PRD.ID', 'UAT.ID'),
-}
+def getFrmList(l=[],i=0):
+    if i >= len(l):
+        return None
+    else:
+        return l[min(i,len(l)-1)]
+
+
 
 def compareXML(jobList_A,jobList_B):
     diff_Jobs= cmpdicts(jobList_A,jobList_B)
@@ -210,7 +172,7 @@ def parseXML_New(xmlName='/dev/null'):
 
 def loopXML(tree=None,cnt=0):
     currLvl=[[{tree.nodeName:dict(tree.attributes.items())}]]
-    print '---'*cnt, "NodeName:",tree.nodeName,"Value:",dict(tree.attributes.items())
+    # print '---'*cnt, "NodeName:",tree.nodeName,"Value:",dict(tree.attributes.items())
     addnAttr=[]
     for childTree in iterate_children(tree):
         childLvl=loopXML(childTree,cnt+1)
@@ -235,9 +197,18 @@ def _sortAtts(x):
     return a
 
 
+mapping={
+    "NODE_ID":('PRD.SERVER','UAT.SERVER'),
+    "USER_ID": ('PRD.ID', 'UAT.ID'),
+}
+
+# a=[0,1,{"NAME":4,'VAR1':1},{"NAME":4,'VAR2':1},4]
+# b=[3,{"NAME":4,'VAR1':2},4,1,5]
+# print cmpDeepLists(a,b)
+# sys.exit(0)
 
 UAT=parseXML_New('./IN_UAT.xml')
-UAT_replace=replaceVerAll(UAT,mapping)
+replaceList(UAT,mapping)
 PRD=parseXML_New('./IN_PRD.xml')
 a= cmpDeepLists(UAT,PRD)
 print a
