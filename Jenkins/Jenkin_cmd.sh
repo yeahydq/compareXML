cccdc5bba1c2b0a9208832edcdebdfa19bd314a5
dick <yeahydq@163.com>
2017-02-18 20:51:52 +0800

    update command

--- Jenkins/Jenkin_cmd.sh
+++ Jenkins/Jenkin_cmd.sh
@@ -15,6 +15,6 @@ java -jar /dick/tool/jenkins_tool/jenkins-cli.jar -s http://localhost:8080 -i /h
 java -jar /dick/tool/jenkins_tool/jenkins-cli.jar -s http://localhost:8080 -i /home/dick/.ssh/key_for_jenkin build freestyle_copy
 
 curl -u dick:f8b6adb2c39ff087ac5b9e07f3b5933a -X get http://localhost:8080/job/freestyle/config.xml -o mylocalconfig.xml
-curl -u dick:f8b6adb2c39ff087ac5b9e07f3b5933a -XPOST http://localhost:8080/createItem?name=freestyle_new_1 --data-binary @mylocalconfig.xml -H "Content-Type:text/xml"
+curl -u dick:f8b6adb2c39ff087ac5b9e07f3b5933a -X POST http://localhost:8080/createItem?name=freestyle_new_1 --data-binary @mylocalconfig.xml -H "Content-Type:text/xml"
 curl -u dick:f8b6adb2c39ff087ac5b9e07f3b5933a -X POST http://localhost:8080/job/freestyle_new_1/build --data-urlencode json='{"parameter": [{"name":"id", "value":"123"}, {"name":"verbosity", "value":"high"}]}'
 
