4b3b0f058ae4e29857f394a75094683459e7ffcd
dick <yeahydq@163.com>
2017-02-19 00:08:27 +0800

    update command

--- Jenkins/Jenkin_cmd.sh
+++ Jenkins/Jenkin_cmd.sh
@@ -18,3 +18,4 @@ curl -u dick:f8b6adb2c39ff087ac5b9e07f3b5933a -X get http://localhost:8080/job/f
 curl -u dick:f8b6adb2c39ff087ac5b9e07f3b5933a -X POST http://localhost:8080/createItem?name=freestyle_new_1 --data-binary @mylocalconfig.xml -H "Content-Type:text/xml"
 curl -u dick:f8b6adb2c39ff087ac5b9e07f3b5933a -X POST http://localhost:8080/job/freestyle_new_1/build --data-urlencode json='{"parameter": [{"name":"id", "value":"123"}, {"name":"verbosity", "value":"high"}]}'
 
+d
